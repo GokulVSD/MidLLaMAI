58e75a5c41f3e3877ef51216f38c68a5e8557c13
GokulVSD <gokul.vasda@gmail.com>
2024-04-08 01:34:41 -0700

    Better prompting

--- 4_bit_quantized.py
+++ 4_bit_quantized.py
@@ -1,22 +1,24 @@
 from transformers import AutoTokenizer
 import transformers
-
-from torch import bfloat16
+from torch import float16
 
 model = "meta-llama/Llama-2-7b-chat-hf"
 
 tokenizer = AutoTokenizer.from_pretrained(model)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
 
 bnb_config = transformers.BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_quant_type='nf4',
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_compute_dtype=bfloat16
+    bnb_4bit_use_double_quant=False,
+    bnb_4bit_compute_dtype=float16
 )
 
 model = transformers.AutoModelForCausalLM.from_pretrained(
     model, 
     quantization_config=bnb_config,
+    device_map={"": 0},
     low_cpu_mem_usage=True
 )
 
@@ -29,8 +31,9 @@ pipeline = transformers.pipeline(
     tokenizer=tokenizer,
 )
 
-sequences = pipeline(
-    'Hello!',
-    do_sample=True
-)
-print(sequences[0].get("generated_text"))
+pipe = transformers.pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=1000)
+
+while True:
+    prompt = input("\nConverse with LLaMA2: ")
+    result = pipe(f"<s>[INST] {prompt} [/INST]")
+    print(result[0]['generated_text'])
